c9e27d7d7ec542635f2f045d4a85a336eaaec285
Tiago Pimentel <tiagopms@gmail.com>
2022-03-04 07:51:22 +0000

    Improve merge script

--- local_scripts/merge_outputs.sh
+++ local_scripts/merge_outputs.sh
@@ -1,21 +1,18 @@
-for seed in 0 1 2
+datasplit=test
+for seed in {5..14}
 do
     for model in 'gpt2' 'gpt2-medium' 'gpt2-large' 'gpt2-xl'
     do
         for top_p in 1.0
         do
-            # python merge_samples.py --data_dir webtext --model ${model} --top_p ${top_p} --datasplit train --seed 0
-            # python merge_samples.py --data_dir webtext --model ${model} --top_p ${top_p} --datasplit valid --seed 0
-            python merge_samples.py --data_dir webtext --model ${model} --top_p ${top_p} --datasplit test --seed ${seed}
+            python merge_samples.py --data_dir webtext --model ${model} --top_p ${top_p} --datasplit ${datasplit} --seed ${seed}
         done
     done
     for model in 'gpt2' 'gpt2-medium'
     do
         for top_p in .9
         do
-            # python merge_samples.py --data_dir webtext --model ${model} --top_p ${top_p} --datasplit train --seed 0
-            # python merge_samples.py --data_dir webtext --model ${model} --top_p ${top_p} --datasplit valid --seed 0
-            python merge_samples.py --data_dir webtext --model ${model} --top_p ${top_p} --datasplit test --seed ${seed}
+            python merge_samples.py --data_dir webtext --model ${model} --top_p ${top_p} --datasplit ${datasplit} --seed ${seed}
         done
     done
     
@@ -23,9 +20,7 @@ do
     do
         for top_p in .95
         do
-            # python merge_samples.py --data_dir webtext --model ${model} --top_p ${top_p} --datasplit train --seed 0
-            # python merge_samples.py --data_dir webtext --model ${model} --top_p ${top_p} --datasplit valid --seed 0
-            python merge_samples.py --data_dir webtext --model ${model} --top_p ${top_p} --datasplit test --seed ${seed}
+            python merge_samples.py --data_dir webtext --model ${model} --top_p ${top_p} --datasplit ${datasplit} --seed ${seed}
         done
     done
 done
